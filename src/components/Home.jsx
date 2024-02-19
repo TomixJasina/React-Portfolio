a1b7093de6b50f2cd00a82b679ac667f69d7cefd
TomixJasina <thomas.jasinski90@gmail.com>
2024-02-19 20:39:41 +0000

    Added styling for Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './Home.css';
 import avatar from '../assets/images/avatar.png'
-import Container from 'react-bootstrap/Container';
+// import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 
 
 const Home = () => {
         return (
-            <Container>
+            <div className='container-fluid'>
             <div className='row mt-5'>
-                <div className='col-6'>
-                <h1 className='text-white'>Welcome to my Portfolio</h1>
-                <p className='text-white'>Learn more about us.</p>
+                <div className='col-xl-6 col-md-6 col-sm-12 mt-5'>
+                <h1 className='text-white mt-5'>Welcome to my Portfolio</h1>
+                <p className='text-white mt-5'>Beyond the screen, I'm driven by a relentless pursuit of innovation, constantly embracing new frameworks and methodologies to craft digital experiences that transcend expectations. Thank you for exploring my portfolio. May it illuminate the possibilities and ignite a shared passion for crafting impactful digital landscapes.</p>
                 </div>
-                <div className='col-2'>
+                <div className='col-xl-6 col-md-6 col-sm-12'>
                 <div>
-                    <Image src={avatar} roundedCircle ></Image>
+                    <Image className='my-5' src={avatar} roundedCircle fluid ></Image>
                 </div>
                 </div>
             </div>
-            </Container>
+            </div>
         );
 }
 
